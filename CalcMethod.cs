9213269b3a230b72aaa5c8c4a62f9b5b5e228171
Pavithra2304 <92969170+Pavithra2304@users.noreply.github.com>
2021-11-19 17:26:32 +0530

    Add files via upload

---
+++ CalcMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class CalcMethod
+    {
+        public  int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public  int Multipication(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
